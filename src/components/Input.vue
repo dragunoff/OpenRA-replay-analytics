82b9c3f74d75da5e89c007b53e212a3a5ca3bb5b
Ivaylo Draganov <dragunoff@gmail.com>
2021-01-08 19:23:01 +0200

    Add support for hashes from ORA Ladder and RAGL

--- src/components/Input.vue
+++ src/components/Input.vue
@@ -3,10 +3,13 @@
     <b-tab title="Upload Replay" :disabled="isInputDisabled && currentTab !== 0">
       <ReplayFileInput />
     </b-tab>
-    <b-tab title="Paste JSON" :disabled="isInputDisabled && currentTab !== 1">
+    <b-tab title="ORA Ladder &amp; RAGL" :disabled="isInputDisabled && currentTab !== 1">
+      <ORALadderInput />
+    </b-tab>
+    <b-tab title="Paste JSON" :disabled="isInputDisabled && currentTab !== 2">
       <ReplayDataInput />
     </b-tab>
-    <b-tab title="Stored Replays" :disabled="isInputDisabled && currentTab !== 2">
+    <b-tab title="Stored Replays" :disabled="isInputDisabled && currentTab !== 3">
       <StoredReplays />
     </b-tab>
   </b-tabs>
@@ -15,12 +18,14 @@
 <script>
 import ReplayDataInput from './ReplayDataInput.vue';
 import ReplayFileInput from './ReplayFileInput.vue';
+import ORALadderInput from './ORALadderInput.vue';
 import StoredReplays from './StoredReplays.vue';
 
 export default {
   components: {
     ReplayDataInput,
     ReplayFileInput,
+    ORALadderInput,
     StoredReplays,
   },
   data() {
---
+++ src/components/ORALadderInput.vue
@@ -0,0 +1,111 @@
+<template>
+  <div class="c-replay-data-input">
+    <b-form @submit="onSubmit($event)">
+      <b-alert variant="danger" dismissible :show="hasError">
+        {{ errorMessage }}
+      </b-alert>
+      <fieldset class="c-replay-file-input__fieldset" :disabled="isInputDisabled">
+        <b-form-group label="Replay hash" label-for="input-replay-hash">
+          <b-form-input
+            id="input-replay-hash"
+            type="text"
+            name="hash"
+            v-model="hash"
+            required
+            :state="isValid"
+          ></b-form-input>
+        </b-form-group>
+        <b-form-group label="Source" label-for="input-replay-hash-source">
+          <b-form-radio-group
+            id="input-replay-hash-source"
+            v-model="source"
+            name="input-replay-hash-source"
+          >
+            <b-form-radio value="oraladder">ORA Ladder</b-form-radio>
+            <b-form-radio value="ragl">RAGL</b-form-radio>
+          </b-form-radio-group>
+        </b-form-group>
+        <Submit />
+      </fieldset>
+    </b-form>
+  </div>
+</template>
+
+<script>
+import { EventBus } from '../event-bus.js';
+import Utils from '../utils.js';
+
+import Submit from './Submit.vue';
+
+export default {
+  components: {
+    Submit,
+  },
+  data() {
+    return {
+      isValid: null,
+      hasError: false,
+      errorMessage: null,
+      hash: null,
+      source: 'oraladder',
+      replayJSON: null,
+    };
+  },
+  computed: {
+    isInputDisabled() {
+      return this.$store.state.settings.isInputDisabled;
+    },
+  },
+  methods: {
+    onSubmit(e) {
+      e.preventDefault();
+
+      if (this.isInputDisabled) {
+        return;
+      }
+
+      this.clearError();
+
+      if (!this.hash) {
+        return;
+      }
+
+      this.$store.commit('settings/setLoadingState', true);
+
+      setTimeout(() => {
+        fetch(`${process.env.VUE_APP_OPENRA_API_ENDPOINT}/${this.source}/${this.hash}`).then(
+          response => response.json()
+        ).then(replayJSON => {
+          this.replayJSON = Utils.cleanUpBuild(replayJSON);
+          this.replayDataReady();
+        }).catch(e => {
+          this.setError(e);
+          this.$store.commit('settings/setLoadingState', false);
+        });
+      }, 0);
+    },
+    replayDataReady() {
+      EventBus.$emit('ReplayDataReady', this.replayJSON);
+      this.$storageManager.storeReplay(this.replayJSON);
+    },
+    setError(e) {
+      this.isValid = false;
+      this.hasError = true;
+      this.errorMessage = `${e.name}: ${e.message}`;
+    },
+    clearError() {
+      this.isValid = null;
+      this.hasError = false;
+      this.errorMessage = null;
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+.c-replay-data-input {
+  &__fieldset {
+    max-width: 50rem;
+  }
+}
+</style>
